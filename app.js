eb3c2f88738b2c95cb1ec54aed9a82ea45a7d9d7
TonyaTer <65990995+TonyaTer@users.noreply.github.com>
2020-09-10 10:00:13 -0400

    adding ejs

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('view engine', 'ejs');
 
 app.get('/', function(req, res){
-  res.send('<h1>Listening</h1>');
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day='';
+  if(currentDay === 6 || currentDay === 0){
+    day = 'Weekend';
+  }else{
+    day = 'Weekday';
+  };
+
+  // res.render('list', {day: day});
 });
 
 app.listen(3000, function(){
