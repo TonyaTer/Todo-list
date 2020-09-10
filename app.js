2b4b40db4fb6f95ef8ba592760587d615a1f8484
TonyaTer <65990995+TonyaTer@users.noreply.github.com>
2020-09-10 13:41:50 -0400

    added stylesheet

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const app = express();
 
-var newItems =[];
-var newItem = '';
+let newItems =["Food", "Cook", "Eat"];
+let newItem = '';
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-  var date = new Date();
+  let date = new Date();
 
-  var options ={
+  let options ={
     weekday: 'long',
     day: 'numeric',
     month: 'long'
   };
 
-  var day = date.toLocaleDateString('en-US', options);
+  let day = date.toLocaleDateString('en-US', options);
 
   res.render('list', {day: day, newListItems: newItems});
 });
